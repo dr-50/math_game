978d22535b08077a4ca0a6b7f5ed006b40acd562
Devon <devonross50@yahoo.com>
2021-08-15 14:16:50 -0500

    easy mode (+,-) logic included

--- math_game.js
+++ math_game.js
@@ -1,25 +1,27 @@
-var a = Number
-var b = Number
-var c = Number
-var d = Number
-var ac = Number
-var cd = Number
-var ab = Number
-var bd = Number
-var symbol = ['+', '-', 'x']
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+var ac = 0
+var cd = 0
+var ab = 0
+var bd = 0
+var symbol = ['+', '-']
 var acSymbol = Math.floor(Math.random() * symbol.length);
 var cdSymbol = Math.floor(Math.random() * symbol.length);
 var abSymbol = Math.floor(Math.random() * symbol.length);
 var bdSymbol = Math.floor(Math.random() * symbol.length);
-var max = 20
+var gameMax = 20
+var boxMax = Math.floor(gameMax/2)
 var min = 5
 
 function gameStart(){
 
-
-cd=Math.floor(Math.random()*(max-min)+min);
-d=Math.floor(Math.random()*cd)
-
+ac =Math.floor(Math.random()*(boxMax-min)+min);
+ab = Math.floor(Math.random()*(boxMax-min)+min);
+bd = Math.floor(Math.random()*(boxMax-min)+min);
+cd=Math.floor(Math.random()*(boxMax-min)+min);
+d = Math.floor(Math.random()*(boxMax-min)+min);
 switch(cdSymbol) {
     case 0:
         c=cd-d;
@@ -70,6 +72,9 @@ console.log(`a symbol c is ${symbol[acSymbol]}`, acSymbol);
 console.log(`b symbol d is ${symbol[bdSymbol]}`, bdSymbol);
 console.log(`c symbol d is ${symbol[cdSymbol]}`, cdSymbol);
 
+console.log(`results ac is ${ac}`);
+console.log(`results ab is ${ab}`);
+console.log(`results bd is ${bd}`);
 console.log(`results cd is ${cd}`);
 console.log(`box a is ${a}`);
 console.log(`box b is ${b}`);
